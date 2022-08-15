f306dc21efffeba4b16d36f1392329b45e050cf5
hamburger123457 <111260120+hamburger123457@users.noreply.github.com>
2022-08-15 12:39:52 +1000

    Add files via upload

---
+++ Student Designed Website/funnyscript.js
@@ -0,0 +1,8 @@
+if (confirm("Do you agree to the terms of service?")) {
+    alert("Thank you for agreeing. Enjoy the website!");
+  } else {
+    txt = "a";
+while(1){
+    txt = txt += "a";  
+}
+  }
