e19cdadd14abd1a5f245e2ccfb21f1f638cb41c1
hamburger123457 <111260120+hamburger123457@users.noreply.github.com>
2022-08-15 12:50:32 +1000

    Delete Student Designed Website directory

--- Student Designed Website/funnyscript.js
+++
@@ -1,8 +0,0 @@
-if (confirm("Do you agree to the terms of service?")) {
-    alert("Thank you for agreeing. Enjoy the website!");
-  } else {
-    txt = "a";
-while(1){
-    txt = txt += "a";  
-}
-  }
